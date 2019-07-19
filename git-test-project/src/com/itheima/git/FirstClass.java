f697029b869a32e6a04b3a54e7e0e26d4d42325b
JJ89757 <jj89757wl@hotmail.com>
2019-07-19 21:02:13 +0800

    修改FirstClass

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -8,6 +8,7 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
+   //hello test
     }
 public void test(){
         int i=0;
