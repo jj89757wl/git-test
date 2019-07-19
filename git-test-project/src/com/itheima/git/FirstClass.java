0b18f77f99366ff82333fd0f509cd9b407541fbd
JJ89757 <jj89757wl@hotmail.com>
2019-07-19 20:51:14 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+/**
+ * @version:1.8
+ * @Auther:JJ89757
+ * @Date:2019/7/19 20:44
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+    }
+}
